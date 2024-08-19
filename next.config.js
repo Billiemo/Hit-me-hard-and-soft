ef5e068fcaa061024e472a1fe8bdd78e03f21fce
Billiemo <mokirpolk@gmail.com>
2024-08-19 10:56:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
-{
-  source: '/old-blog/hit-me-hard-and-soft.vercel.app:path*',
-  destination: '/blog/https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb:path*',
-  permanent: false
-}
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/old-blog/:slug',
+        destination: '/news/:slug',
+        permanent: true,
+      },
+      {
+        source: '/:path((?!uk/).*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-vercel-ip-country',
+            value: 'GB',
+          },
+        ],
+        permanent: false,
+        destination: 'https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb',
+      },
+    ];
+  },
+};
