fc42b19c716d3fa2e948f2f35e91cd0497f0226a
Billiemo <mokirpolk@gmail.com>
2024-08-19 10:48:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 {
-  source: '/old-blog/:path*',
+  source: '/old-blog/hit-me-hard-and-soft.vercel.app:path*',
   destination: '/blog/https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb:path*',
   permanent: false
 }
