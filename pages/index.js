8d3e073b16b847549c31de4870855e37e311f648
Billiemo <mokirpolk@gmail.com>
2024-09-09 19:05:32 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <meta property="og:image" content="https://0.soompi.io/wp-content/uploads/sites/8/2024/09/06060841/Uprising.jpg" />
+        <meta property="og:title" content="Redirecting..." />
+        <meta property="og:description" content="You are being redirected." />
+      </Head>
+      <h1>Redirecting...</h1>
+    </div>
+  )
+}
